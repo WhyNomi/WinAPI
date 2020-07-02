0252dc579346948c56d159bc741ed4b2ad7f54d5
Young-Yoshi <dener.1232@gmail.com>
2020-07-02 18:39:35 +0300

    GetNumber

---
+++ Calc/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON10                    1004
+#define IDC_BUTTON3                     1005
+#define IDC_BUTTON4                     1006
+#define IDC_BUTTON5                     1007
+#define IDC_BUTTON6                     1008
+#define IDC_BUTTON7                     1009
+#define IDC_BUTTON8                     1010
+#define IDC_BUTTON9                     1011
+#define IDC_BUTTON_PLUS                 1012
+#define IDC_BUTTON_MINUS                1013
+#define IDC_BUTTON_ASTER                1014
+#define IDC_BUTTON0                     1015
+#define IDC_BUTTON15                    1016
+#define IDC_BUTTON_SLASH                1017
+#define IDC_BUTTON_POINT                1018
+#define IDC_BUTTON11                    1019
+#define IDC_BUTTON_CLEAR                1020
+#define IDC_BUTTON_RESULT               1021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
